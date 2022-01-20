7af057959eba06f66c5ab1d9269be11debcce03a
rasset69@gmail.com <rasset69@gmail.com>
2022-01-19 18:00:53 -0700

    Add Header Add Comments

--- SethRas_Say_My_Name/SayMyName.vb
+++ SethRas_Say_My_Name/SayMyName.vb
@@ -1,19 +1,27 @@
-﻿Module SayMyName
+﻿'Seth Nieslen Rasmussen
+'RCET0265
+'Spring 2022
+'Say My Name
+
+Option Explicit On
+Option Strict On
+
+Module SayMyName
 
     Sub Main()
-
+        'Declare UserInput as a variable that the code will read
         Dim userInput As String
 
+        'Upon running the program this line will prompt the user to input their name
         Console.WriteLine("Welcome to Skynet, What is your name?")
         userInput = Console.ReadLine()
 
-        'show the user what they just typed 
+        'show the user what they just typed with a greeting
         Console.WriteLine("Hello " & userInput & ", Welcome to the Skynet Automated Defense Network")
 
         'This puts a pause in the program until the user has an input
         Console.ReadLine()
 
-        Console.WriteLine("")
     End Sub
 
 End Module

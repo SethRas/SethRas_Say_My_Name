5a71793117d6bcc83cf581d3866740e4eb804bf6
rasset69@gmail.com <rasset69@gmail.com>
2022-01-19 17:40:54 -0700

    Add project files.

---
+++ SethRas_Say_My_Name/SayMyName.vb
@@ -0,0 +1,19 @@
+﻿Module SayMyName
+
+    Sub Main()
+
+        Dim userInput As String
+
+        Console.WriteLine("Welcome to Skynet, What is your name?")
+        userInput = Console.ReadLine()
+
+        'show the user what they just typed 
+        Console.WriteLine("Hello " & userInput & ", Welcome to the Skynet Automated Defense Network")
+
+        'This puts a pause in the program until the user has an input
+        Console.ReadLine()
+
+        Console.WriteLine("")
+    End Sub
+
+End Module
